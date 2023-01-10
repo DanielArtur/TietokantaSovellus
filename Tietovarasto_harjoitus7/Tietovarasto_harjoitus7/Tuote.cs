5fb332b6d6cacf9f6358595c63bcd829567211db
Tervo Daniel <s2001075@edu.bc.fi>
2023-01-10 16:57:48 +0200

    Valmis

--- Tietovarasto_harjoitus7/Tietovarasto_harjoitus7/Tuote.cs
+++ Tietovarasto_harjoitus7/Tietovarasto_harjoitus7/Tuote.cs
@@ -1,19 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.ComponentModel.DataAnnotations;
 
 namespace Tietovarasto_harjoitus7
 {
     public class Tuote
     {
+
         [Key]
-        public string? Tuote_ID { get; set; }
+        public string? Id { get; set; }
         public string? Tuotenimi { get; set; }
         public string? Tuotehinta { get; set; }
         public string? Varastosaldo { get; set; }
+
+
 
     }
 }
